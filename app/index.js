c588143173570c987eb4645edc4de29f5f36778a
Bryan <bryan.davila@charismamedia.com>
2016-09-22 15:14:13 -0400

    added some components

--- app/index.js
+++ app/index.js
@@ -1,3 +1,75 @@
-var app = document.getElementById('app');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-app.innerHTML = 'Hello';
+var CommentBox = React.createClass({
+  render: function () {
+    return (
+      <div className="commentBox">
+        <h1>Email Form</h1>
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function () {
+    return (
+      <div className="commentList">
+        <Comment author="bryan">This is my comment!</Comment>
+      </div>
+    )
+  }
+});
+
+var CommentForm = React.createClass({
+
+  getInitialState: function () {
+    return {email: ''};
+  },
+
+  handleEmailChange: function (e) {
+    //console.log(e.target.value);
+    this.setState({email: e.target.value});
+  },
+
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var email = this.state.email.trim();
+    console.log(email);
+    if(!email){
+      return;
+    }
+    this.setState({email: ''});
+  },
+
+  render: function () {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text"
+               placeholder="Enter your email address"
+               value={this.state.email}
+               onChange={this.handleEmailChange} />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('app')
+);
